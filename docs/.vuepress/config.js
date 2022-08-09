9d24ef0343ba02e31db7781ba9f8ca0d86a1a0e4
张荣侨 <zrq21@mails.tsinghua.edu.cn>
2022-08-09 11:37:26 +0800

    转换为VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+/*
+ * @Author       : randolf
+ * @Date         : 2022-08-09 11:28:15
+ * @LastEditors  : randolf
+ * @LastEditTime : 2022-08-09 11:35:40
+ * @FilePath     : \randolfly.github.io\docs\.vuepress\config.js
+ */
+module.exports = {
+  title: "天策",
+  description: "randolf blog",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "More",
+        items: [{ text: "Github", link: "https://github.com/randolfly" }],
+      },
+    ],
+  },
+};
